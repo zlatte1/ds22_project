e01b47b136cd45f5e66e8570d490eb76f7c4437a
Anders Pettersson <57747360+zlatte1@users.noreply.github.com>
2023-04-14 02:51:10 +0200

    added eces code to streamlit

--- streamlit/pages/03_Model_Development.py
+++ streamlit/pages/03_Model_Development.py
@@ -1,8 +1,21 @@
 import streamlit as st
+from PIL import Image
+image = Image.open('./images/model_dev_01.png')
+
+
+list_of_bullets = [
+    '- Models applied initially : Linear regression, polynomial regression, KNN, SVR, DT, For., Lasso regression, Ensemble models (voting and bagging regressor) and Neural network (keras)',
+    '- Scaled on RobustScaler for optimal coefficients and have better performance',
+    '- checked: performance metrics of R2, RMSE, mse, mae'
+]
 
 def main():
     st.title("Model Development")
-    st.write("")
+    for item in list_of_bullets:
+        st.write(item)
+
+    st.subheader("Here are initial results of ML models:")
+    st.image(image, caption='')
 
 if __name__ == '__main__':
     main()
--- streamlit/pages/04_Model_Evalulation.py
+++ streamlit/pages/04_Model_Evalulation.py
@@ -1,8 +1,22 @@
 import streamlit as st
+from PIL import Image
+image = Image.open('./images/model_dev_02.png')
+image_02 = Image.open('./images/model_dev_03.png')
+image_03 = Image.open('./images/model_dev_04.png')
+
+
 
 def main():
     st.title("Model Evalulation")
-    st.write("")
+    st.write("Models hypertuned after Grid search & performance metrics")
+    st.image(image, caption='')
+
+    st.subheader("Plotting train, test val splits")
+    st.write("Polynomial regressor")
+    st.image(image_02, caption='')
+
+    st.write("Polynomial regressor visuals")
+    st.image(image_03, caption='')
 
 if __name__ == '__main__':
     main()
