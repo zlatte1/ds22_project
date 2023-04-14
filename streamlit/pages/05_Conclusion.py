d3849fd8eb00fc118160dca2cd23849e30e8ff0b
Anders Pettersson <57747360+zlatte1@users.noreply.github.com>
2023-04-14 02:58:25 +0200

    Update 05_Conclusion.py

--- streamlit/pages/05_Conclusion.py
+++ streamlit/pages/05_Conclusion.py
@@ -2,7 +2,15 @@ import streamlit as st
 
 def main():
     st.title("Conclusion")
-    st.write("")
+    st.write("ThougtS:")
+    st.write("Limitaton of the model:")
+    st.write("Basically since the dataset is top100 movies from the last 20y, its going to be able to predict blockbusters pretty well but not horror, doc, bollywood or foreign")
+    st.write("What would we do if we had more time and resources")
+    st.write("scrape all movies on imdb and maybe even add a second source of data -- RT")
+    st.write("Data-nice-to-have:")
+    st.write("film_company:")
+    st.write("ThougtS:")
+    st.write(":")
 
 if __name__ == '__main__':
     main()
