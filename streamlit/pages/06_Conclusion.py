4a9bb9ac3a8376b293a6180521eb695c663716e0
Anders Pettersson <57747360+zlatte1@users.noreply.github.com>
2023-04-15 03:23:53 +0200

    Update 06_Conclusion.py

--- streamlit/pages/06_Conclusion.py
+++ streamlit/pages/06_Conclusion.py
@@ -3,18 +3,19 @@ from PIL import Image
 
 def main():
     st.title("Conclusion")
-    st.write("ThougtS:")
-    st.write("Limitaton of the model:")
-    st.write("Basically since the dataset is top100 movies from the last 20y, its going to be able to predict blockbusters pretty well but not horror, doc, bollywood or foreign")
-    st.write("What would we do if we had more time and resources")
-    st.write("scrape all movies on imdb and maybe even add a second source of data -- RT")
-    st.write("Data-nice-to-have:")
-    st.write("film_company:")
-    st.write("ThougtS:")
-    st.write("out of a buisness/ client perspective it would make more sense to predict income or ROI ")
-    st.write("and what features has the highest co-eff to this")
-
-    image_0 = Image.open('./images/conclusion_pred.png')
+ 
+    image_0 = Image.open('../images/conclusion_pred.png')
     st.image(image_0)
+
+    st.subheader("Purpose")
+    st.write("Using Machine Learning to Predict Movie Ratings")
+
+    st.subheader("How did we do?")
+    st.write("-- Perfomance Metrics/ Models")
+    st.write("-- Limitations")
+    st.write("-- What would we do diffrently")
+
+    st.write("Summarize what the product actually do")
+
 if __name__ == '__main__':
     main()
